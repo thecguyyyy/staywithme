938ae808bad95fe37ddf0ac7901fee565c2595ea
thecguyyyy <chenqiyuan0624@gmail.com>
2026-06-16 20:08:52 +0800

    Align companion player render layers

--- src/main/java/com/thecguyyyy/staywithme/entity/FriendRenderer.java
+++ src/main/java/com/thecguyyyy/staywithme/entity/FriendRenderer.java
@@ -1,16 +1,30 @@
 package com.thecguyyyy.staywithme.entity;
 
 import com.thecguyyyy.staywithme.client.CompanionSkinTextures;
-import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.HumanoidArmorModel;
+import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
+import net.minecraft.client.renderer.entity.layers.ArrowLayer;
+import net.minecraft.client.renderer.entity.layers.BeeStingerLayer;
+import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
+import net.minecraft.client.renderer.entity.layers.SpinAttackEffectLayer;
 import net.minecraft.client.resources.DefaultPlayerSkin;
 import net.minecraft.resources.ResourceLocation;
 
-public class FriendRenderer extends HumanoidMobRenderer<FriendEntity, HumanoidModel<FriendEntity>> {
+public class FriendRenderer extends HumanoidMobRenderer<FriendEntity, PlayerModel<FriendEntity>> {
     public FriendRenderer(EntityRendererProvider.Context context) {
-        super(context, new HumanoidModel<>(context.bakeLayer(ModelLayers.PLAYER)), 0.5F);
+        super(context, new PlayerModel<>(context.bakeLayer(ModelLayers.PLAYER), false), 0.5F);
+        this.addLayer(new HumanoidArmorLayer<>(
+                this,
+                new HumanoidArmorModel<>(context.bakeLayer(ModelLayers.PLAYER_INNER_ARMOR)),
+                new HumanoidArmorModel<>(context.bakeLayer(ModelLayers.PLAYER_OUTER_ARMOR)),
+                context.getModelManager()
+        ));
+        this.addLayer(new ArrowLayer<>(context, this));
+        this.addLayer(new SpinAttackEffectLayer<>(this, context.getModelSet()));
+        this.addLayer(new BeeStingerLayer<>(this));
     }
 
     @Override
